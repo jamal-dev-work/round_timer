f746af4689f6cd9b09e6f93483743664aed94203
jamal-dev-work <jamal.dev.work@gmail.com>
2026-02-09 22:01:50 +0200

    add WorkoutConfig class to manage workout settings

---
+++ lib/features/workout/model/workout_config.dart
@@ -0,0 +1,11 @@
+class WorkoutConfig {
+  final int workoutSeconds;
+  final int restSeconds;
+  final int rounds;
+
+  WorkoutConfig({
+    required this.workoutSeconds,
+    required this.restSeconds,
+    required this.rounds,
+  });
+}
