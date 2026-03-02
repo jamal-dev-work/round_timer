474e4611073cacbc3eaee255de352d398a04941c
jamal-dev-work <jamal.dev.work@gmail.com>
2026-03-02 21:38:18 +0200

    update workout provider

--- lib/features/workout/provider/workout_provider.dart
+++ lib/features/workout/provider/workout_provider.dart
@@ -3,6 +3,9 @@ import 'package:round_timer/features/workout/model/workout_config.dart';
 import 'package:round_timer/features/workout/model/workout_state.dart';
 
 class WorkoutProvider extends ChangeNotifier {
+  int number1 = 0;
+  int number2 = 0;
+  int number3 = 0;
   // late WorkoutConfig _config;
   // Timer? _timer;
   WorkoutState _state = WorkoutState(
@@ -22,6 +25,12 @@ class WorkoutProvider extends ChangeNotifier {
       phase: WorkoutPhase.workout,
       isRunning: false,
     );
+    notifyListeners();
+  }
+  void setNumbers(int n1, int n2 , int n3) {
+    number1 = n1;
+    number2 = n2;
+    number3 = n3;
     notifyListeners();
   }
 }
